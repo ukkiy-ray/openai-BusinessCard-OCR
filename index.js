25282c9827285625fbf477aebf892af5315117d9
Utsuki Sho <showy13f207ag@gmail.com>
2024-10-02 01:07:09 +0900

    openai apiの初期化処理をモジュールに移管

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-require("dotenv").config();
-const OpenAI = require("openai");
+"use strict";
 const openaiApiProcess = require("./lib/openai");
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-
-const client = new OpenAI({
-  apiKey: OPENAI_API_KEY,
-});
-
 const message = "open AI とは?";
-
-console.log(new openaiApiProcess(client, message).chat());
+console.log(new openaiApiProcess(message).chat());
