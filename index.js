8fa34e40479bbce1d4a3a1fe77ec73ee93ef6377
Utsuki Sho <showy13f207ag@gmail.com>
2024-10-02 00:48:20 +0900

    API呼び出し部分をモジュール化

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const OpenAI = require("openai");
+const openaiApiProcess = require("./lib/openai");
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
@@ -7,17 +8,6 @@ const client = new OpenAI({
   apiKey: OPENAI_API_KEY,
 });
 
-async function getCompletion() {
-  try {
-    const completion = await client.chat.completions.create({
-      messages: [{ role: "user", content: "Say this is a test" }],
-      model: "gpt-3.5-turbo",
-    });
+const message = "open AI とは?";
 
-    console.log(completion.choices[0].message.content); // .contentにアクセス
-  } catch (error) {
-    console.error("Error fetching completion:", error);
-  }
-}
-
-getCompletion(); // 関数を呼び出す
+console.log(new openaiApiProcess(client, message).chat());
