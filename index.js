411a10a089c7d088c022760c14c87095410c86e0
Utsuki Sho <showy13f207ag@gmail.com>
2024-10-01 23:26:15 +0900

    chat.completions呼び出し

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello OpenAI");
+require("dotenv").config();
+const OpenAI = require("openai");
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const client = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+});
+
+async function getCompletion() {
+  try {
+    const completion = await client.chat.completions.create({
+      messages: [{ role: "user", content: "Say this is a test" }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(completion.choices[0].message.content); // .contentにアクセス
+  } catch (error) {
+    console.error("Error fetching completion:", error);
+  }
+}
+
+getCompletion(); // 関数を呼び出す
